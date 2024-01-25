ce909e2d9ed28fb3c3faf05a7587477ae4c7188c
frugiam <frugiam@oregonstate.edu>
2024-01-24 18:56:03 -0800

    min_max.py

---
+++ min_max.py
@@ -0,0 +1,19 @@
+# Author: Michelle Frugia
+# GitHub username: frugiam
+# Date: 01/23/2024
+# Description: Project 3a
+
+print("How many integers would you like to enter?")
+num_integers = int(input())
+min_num = 0
+max_num = 0
+print("Please enter", num_integers, "integers.")
+for i in range(num_integers):
+    num = int(input())
+    if num < min_num:
+        min_num = num
+    if num > max_num:
+        max_num = num
+
+print("min:", min_num)
+print("max:", max_num)
